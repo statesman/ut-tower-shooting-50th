52a2f725a519426ec807b89ed1432748e5f92145
Cody Winchester <cjwinchester@gmail.com>
2016-05-24 12:06:35 -0500

    map player controls are hooked up now

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
 // set global template variable
 _.templateSettings.variable = "template_data";
+
+// jank pluralizer function
+var pluralize = function(num) {
+    if (num === 1) {
+        return ["", "the"];
+    } else {
+        return ["s", "each"];
+    }
+};
