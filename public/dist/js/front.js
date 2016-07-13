c401090283fbc9e984d3d32dbd40dd7f4e586fe6
critmcdonald <christian@cmcdonald.com>
2016-07-13 13:25:27 -0500

    adding last link

--- public/dist/js/front.js
+++ public/dist/js/front.js
@@ -2,37 +2,37 @@
 
 $(document).ready(function() {
 
-// do up masonry
-var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item'
-});
-
-$grid.imagesLoaded().progress(function() {
-  $grid.masonry('reloadItems')
-       .masonry('layout');
-});
-
-
-var myPlayer;
-
-function changeVideo(video_id){
-    myPlayer.catalog.getVideo(video_id, function(error, video) {
-        myPlayer.catalog.load(video);
-        myPlayer.play();
+    // do up masonry
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item'
     });
-}
 
-videojs("video-player-front").ready(function(){
-    myPlayer = this;
-});
+    $grid.imagesLoaded().progress(function() {
+      $grid.masonry('reloadItems')
+           .masonry('layout');
+    });
 
-$('.video-tease-wrapper').on('click', function() {
-    var vid_id = $(this).data("video-id");
-    changeVideo(vid_id);
-    // remove "active" state from teases, add to this one
-    $('.video-tease-wrapper').removeClass('video-tease-active');
-    $(this).addClass('video-tease-active');
-});
+
+    var myPlayer;
+
+    function changeVideo(video_id){
+        myPlayer.catalog.getVideo(video_id, function(error, video) {
+            myPlayer.catalog.load(video);
+            myPlayer.play();
+        });
+    }
+
+    videojs("video-player-front").ready(function(){
+        myPlayer = this;
+    });
+
+    $('.video-tease-wrapper').on('click', function() {
+        var vid_id = $(this).data("video-id");
+        changeVideo(vid_id);
+        // remove "active" state from teases, add to this one
+        $('.video-tease-wrapper').removeClass('video-tease-active');
+        $(this).addClass('video-tease-active');
+    });
 
 
 });
