addbc1074c9633a534d090834b909840d5ac3a8e
Cody Winchester <cjwinchester@gmail.com>
2016-07-13 10:29:36 -0500

    front page switcher dude working on IE9-IE11 + iphone user agent, need to try actual phone + ipad

--- public/dist/js/front.js
+++ public/dist/js/front.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
                     }
                 });
 
-                        /*
+                    // ----- nooooope ----- //
                         // fetch the video from brightcove
                         self.catalog.getVideo(String(new_video_id), function (error, video) {
                             if (error) {
@@ -40,17 +40,53 @@ $(document).ready(function() {
     });
     */
 
+
+
+    /*
+     * pause all brightcove player instances except for the one with the video matching the ID
+     *
+     * @param {String} target_video_id - the ID of the video not to pause
+     */
+    function pause_other_video_players(target_video_id) {
+        // loop over the video players
+        $('.vjs-tech').each(function() {
+            // get the ID of the video
+            var this_video_id = $(this).data('video-id');
+
+            // if it doesn't match the target video
+            if (this_video_id !== target_video_id) {
+
+                // get videojs instance
+                var pause_this_player = videojs([
+                    "video-player-front-",
+                    this_video_id,
+                    "_html5_api"
+                ].join(""));
+
+                // if it's playing, pause it
+                if (!pause_this_player.paused()) {
+                    pause_this_player.pause();
+                }
+            }
+        });
+    }
+
     // click event for video tease - load that video
     $('.video-tease-wrapper').on('click', function() {
         // get the ID of the new video
         var new_video_id = $(this).data('video-id');
+
+        // move it onscreen
+        // v sorry about this css everyone
         $(".video-player-front").addClass("offscreen");
         $("#video-player-wrapper-" + new_video_id).removeClass("offscreen");
+
+        // pause other players
+        pause_other_video_players(new_video_id);
 
         // remove "active" state from teases, add to this one
         $('.video-tease-wrapper').removeClass('video-tease-active');
         $(this).addClass('video-tease-active');
-
     });
 
     // do up masonry
